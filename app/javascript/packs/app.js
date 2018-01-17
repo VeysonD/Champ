5b9611a52a500756c07d5a6f8fcb46b5f1a5b16d
Rick Gallegos <duulketariakan@yahoo.com>
2018-01-17 14:55:47 -0800

    Add API calls to PostBoard component and style it with material UI

--- app/javascript/packs/app.js
+++ app/javascript/packs/app.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch, Link } from 'react-router-dom';
-import axios from 'axios';
 
 import PostBoard from '../src/components/PostBoard';
 import Post from '../src/components/Post';
@@ -12,16 +11,6 @@ import EditPost from '../src/components/EditPost';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      allPosts: [],
-    };
-  }
-  componentWillMount() {
-    axios
-      .get('/api/posts')
-      .then(posts => this.state.allPosts = posts)
-      .catch(error => console.error(error));
-    console.log('what is the state:', this.state);
   }
 
   render() {
@@ -29,13 +18,12 @@ export default class App extends Component {
       <BrowserRouter>
         <div>
           <Switch>
-            <Route exact path="/" component= { PostBoard } />
+            <Route exact path="/" component={ PostBoard } />
             <Route path="/posts/new" component={ NewPost } />
             <Route path="/posts/:id" component={ Post } />
             <Route path="/posts/:id/edit" component={ EditPost } />
           </Switch>
         </div>
-
       </BrowserRouter>
     )
   }
--- app/javascript/src/components/PostBoard.jsx
+++ app/javascript/src/components/PostBoard.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Paper from 'material-ui/Paper';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import RaisedButton from 'material-ui/RaisedButton';
@@ -17,6 +18,18 @@ import { Card, CardTitle } from 'material-ui/Card';
 class PostBoard extends Component {
   constructor (props) {
     super(props);
+    this.state = {
+      allPosts: null,
+    };
+  }
+  componentWillMount() {
+    axios.get('/api/posts').then(posts => {
+      console.log('What are the posts 1:', posts);
+      this.setState({ allPosts: posts.data })
+      console.log('what is the state 1:', this.state);
+    })
+    .catch(error => console.error(error));
+    console.log('what is the state 2:', this.state);
   }
   render() {
     return (
@@ -42,8 +55,29 @@ class PostBoard extends Component {
                   <TableHeaderColumn>Delete</TableHeaderColumn>
                 </TableRow>
               </TableHeader>
-              <TableBody stripedRows displayRowCheckbox={false}>
-              </TableBody>
+              { this.state.allPosts
+                ?
+                <TableBody stripedRows displayRowCheckbox={false}>
+                  {this.state.allPosts.map(post => (
+                    <TableRow key={post.id}>
+                      <TableRowColumn>{post.title} {post.factorial}</TableRowColumn>
+                      <TableRowColumn>{post.body}</TableRowColumn>
+                      { post.published
+                        ?
+                        <TableRowColumn>T</TableRowColumn>
+                        :
+                        <TableRowColumn>F</TableRowColumn>
+                      }
+                      <TableHeaderColumn>Show</TableHeaderColumn>
+                      <TableHeaderColumn>Edit</TableHeaderColumn>
+                      <TableHeaderColumn>Delete</TableHeaderColumn>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                :
+                <TableBody stripedRows displayRowCheckbox={false}>
+                </TableBody>
+              }
             </Table>
           </Card>
           <RaisedButton
